5d61e3b1e3687ad80997bbeb570c7b125cbf9868
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-08-15 15:32:15 -0400

    added historyService

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,4 +1,6 @@
 // TODO: Define a City class with name and id properties
+import fs, { readFile } from "node:fs";
+
 class City {
   private baseURL?: string;
 
@@ -14,11 +16,30 @@ class City {
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    fs.readFile("../db/searchHistory.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      return data;
+    });
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+private async write(cities: City[]) {
+  fs.writeFile('../dp/searchHistory.json', JSON.stringify(cities), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
+}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    let cities = await this.read()
+    return cities
+  };
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
